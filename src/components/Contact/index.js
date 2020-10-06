126058ddc1cc3c0c993bfe3717c59f96b4941364
davidbelleza <davidbelleza@gmail.com>
2020-10-05 23:07:59 -0500

    add gh-pages

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -14,7 +14,7 @@ function Contact() {
 
     return (
         <section>
-          <h1>Contact me</h1>
+          <h2>Contact me</h2>
           <form id="contact-form">
             <div>
                 <label htmlFor="name">Name:</label>
