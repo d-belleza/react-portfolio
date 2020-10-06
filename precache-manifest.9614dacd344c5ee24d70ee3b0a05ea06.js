48c9dbd38fbf5e9f09ee66f131bba711578a1807
davidbelleza <davidbelleza@gmail.com>
2020-10-05 23:07:11 -0500

    Updates

---
+++ precache-manifest.9614dacd344c5ee24d70ee3b0a05ea06.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8e527c1601a37af4a946ddc6afb01e1",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "2957e13b9ee6c1c30572",
+    "url": "/react-portfolio/static/css/main.7b7a86ea.chunk.css"
+  },
+  {
+    "revision": "24ddec1d92d946323b7d",
+    "url": "/react-portfolio/static/js/2.6e86267e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.6e86267e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2957e13b9ee6c1c30572",
+    "url": "/react-portfolio/static/js/main.5b12f431.chunk.js"
+  },
+  {
+    "revision": "42e6e07fd3dcb766db6b",
+    "url": "/react-portfolio/static/js/runtime-main.14f01a53.js"
+  },
+  {
+    "revision": "90a66e05f56debefcf8cb2bf9f6e92e7",
+    "url": "/react-portfolio/static/media/D_Belleza_Resume.90a66e05.pdf"
+  },
+  {
+    "revision": "db12c63d8fbfd7834a1b61e6c3a76912",
+    "url": "/react-portfolio/static/media/app-1.db12c63d.png"
+  },
+  {
+    "revision": "8960a596599a264e84f6eccfbe1cb0da",
+    "url": "/react-portfolio/static/media/app-2.8960a596.png"
+  },
+  {
+    "revision": "b4bc3492adbb5276817ebe95cd4096b6",
+    "url": "/react-portfolio/static/media/app-3.b4bc3492.png"
+  },
+  {
+    "revision": "ec595e6e745b636787237d303ef12264",
+    "url": "/react-portfolio/static/media/app-4.ec595e6e.png"
+  },
+  {
+    "revision": "fd245de7790168f6e263cbfeb362d413",
+    "url": "/react-portfolio/static/media/headshot.fd245de7.JPG"
+  }
+]);
